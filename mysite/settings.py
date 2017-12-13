b15d08ce65b28fac72d2138e490f796c94052c52
hora2017 <ho_ra@naver.com>
2017-12-13 16:51:51 +0900

    프로젝트명 수정/다이어리 앱 시작

--- mysite/settings.py
+++ mysite/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'diary.urls'
+ROOT_URLCONF = 'mysite.urls'
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'diary.wsgi.application'
+WSGI_APPLICATION = 'mysite.wsgi.application'
 
 
 # Database
