99973750b2cba7999bce9b861039e6bc965b9d44
hora2017 <ho_ra@naver.com>
2017-12-14 12:50:53 +0900

    글쓰기 모델 생성

--- diary/models.py
+++ diary/models.py
@@ -1,3 +1,19 @@
+from django.utils import timezone
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User')
+    text1 = models.TextField()
+    text2 = models.TextField()
+    text3 = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+
+    def publish(self):
+        self.save()
+
+    def __ref__(self):
+        return self.created_date
